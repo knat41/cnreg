4194c1088d9fe8ba5bc45154c5cc5e2431b3b563
knat41 <75963145+knat41@users.noreply.github.com>
2023-10-12 15:07:26 +0700

    Update test_pd.py

--- test_pd.py
+++ test_pd.py
@@ -1,5 +1,6 @@
-# Used to clean and analyse the data-set
+# Used to analyse the data-set
 import pandas as pd
-# To open an excel file
+# To open an excel file but contain html code
 dfm = pd.read_html('m3.xls', encoding = 'utf-8')
+# change list to dataframe
 df = dfm[0]
