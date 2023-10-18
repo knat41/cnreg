d242badd0d7b38b556b530d162a67f3c1b599f76
knat41 <75963145+knat41@users.noreply.github.com>
2023-10-18 16:42:58 +0700

    Update test_pd.py

--- test_pd.py
+++ test_pd.py
@@ -1,6 +1,19 @@
-# Used to analyse the data-set
+# Used to clean and analyse the data-set
 import pandas as pd
-# To open an excel file but contain html code
-dfm = pd.read_html('m3.xls', encoding = 'utf-8')
-# change list to dataframe
-df = dfm[0]
+
+datalist = ['m3.xls', 'm3-1.xls']
+dflist = []
+
+for data in datalist:
+    # To open an excel file with encoding utf-8 for read thai
+    dfm = pd.read_html(data, encoding = 'utf-8')
+    # append data frame to dataframe list
+    dflist.append(dfm[0])
+
+all_student = pd.concat(dflist)
+
+#convert data type
+all_student.astype({'เลขประขำตัว':'str'}).dtypes
+
+studentID = all_student.loc[all_student['เลขประขำตัว'] == 42540]
+
