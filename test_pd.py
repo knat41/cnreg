dc1351af55fa3647ebc2fc273ca783f6787ced1f
knat41 <75963145+knat41@users.noreply.github.com>
2023-10-19 11:03:04 +0700

    ADD variable

--- test_pd.py
+++ test_pd.py
@@ -1,8 +1,13 @@
 # Used to clean and analyse the data-set
 import pandas as pd
 
+from cn_var import *
+
 datalist = ['m3.xls', 'm3-1.xls']
 dflist = []
+
+print('Start create dataframe')
+print('....')
 
 for data in datalist:
     # To open an excel file with encoding utf-8 for read thai
@@ -12,10 +17,26 @@ for data in datalist:
 
 all_student = pd.concat(dflist)
 
+print('created done')
+
 #convert data type
 all_student.astype({'เลขประขำตัว':'str'}).dtypes
 
 studentID = all_student.loc[all_student['เลขประขำตัว'] == 42540]
 
-#select with condition
 carryCourse = studentID.loc[studentID['รหัสวิชา'].str.contains(".*ว.*")]
+'''
+how to save to excel
+Create, write to and save a workbook:
+carryCourse.to_excel("output.xlsx")
+
+To specify the sheet name:
+df1.to_excel("output.xlsx",
+             sheet_name='Sheet_name_1')
+
+If you wish to write to more than one sheet in the workbook,
+it is necessary to specify an ExcelWriter object:
+with pd.ExcelWriter('output.xlsx') as writer:  
+    df1.to_excel(writer, sheet_name='Sheet_name_1')
+    df2.to_excel(writer, sheet_name='Sheet_name_2')
+'''
